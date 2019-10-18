301c275738d770389c770a4aa0baa25523396d9a
leigh-perry <lperry.breakpoint@gmail.com>
2019-10-18 03:38:51 +0000

    Add scalacheck test for Log

---
+++ modules/core/src/test/scala/com/leighperry/log4zio/LogTest.scala
@@ -0,0 +1,57 @@
+package com.leighperry.log4zio
+
+import com.leighperry.log4zio.Log.{ LogStep, Service }
+import com.leighperry.log4zio.testsupport.TestSupport
+import org.scalacheck.{ Gen, Properties }
+import zio.{ Ref, ZIO }
+
+object LogTest extends Properties("LogTest") with TestSupport {
+
+  final case class LogData(
+    appName: Option[String],
+    logStrings: List[String],
+    logFn: (Log, String) => ZIO[Any, Nothing, Unit],
+    logStep: String => LogStep
+  )
+
+  private val genLogData =
+    for {
+      appName <- Gen.option(genFor[String])
+      scount <- Gen.chooseNum[Int](0, 20)
+      strings <- Gen.listOfN(scount, genFor[String])
+      logAction <- Gen.oneOf(
+        ((log: Log, s: String) => log.log.error(s)) -> ((s: String) => Log.Error(() => s): LogStep),
+        ((log: Log, s: String) => log.log.warn(s)) -> ((s: String) => Log.Warn(() => s): LogStep),
+        ((log: Log, s: String) => log.log.info(s)) -> ((s: String) => Log.Info(() => s): LogStep),
+        ((log: Log, s: String) => log.log.debug(s)) -> ((s: String) => Log.Debug(() => s): LogStep)
+      )
+    } yield LogData(appName, strings, logAction._1, logAction._2)
+
+  property("console") = forAllZIO(genLogData) {
+    ld =>
+      for {
+        dc <- dummyConsole(ld.appName)
+        (entries, log) = dc
+        _ <- ZIO.traverse_(ld.logStrings)(s => ld.logFn(log, s))
+        strings <- entries.get
+        expected = ld.logStrings.map(s => formatMessage(ld.appName, ld.logStep(s))).reverse
+      } yield strings.shouldBe(expected)
+  }
+
+  private def dummyConsole(prefix: Option[String]): ZIO[Any, Nothing, (Ref[List[String]], Log)] =
+    for {
+      entries <- Ref.make(List.empty[String])
+      log <- ZIO.effectTotal {
+        new Log {
+          override def log: Service =
+            new Service {
+              override def log: LogStep => ZIO[Any, Nothing, Unit] =
+                l => entries.update(formatMessage(prefix, l) :: _).unit
+            }
+        }
+      }
+    } yield (entries, log)
+
+  private def formatMessage(prefix: Option[String], l: LogStep) =
+    "%-5s - %s%s".format(l.level, prefix.fold("")(s => s"$s: "), l.message())
+}
--- modules/core/src/test/scala/com/leighperry/log4zio/testsupport/TestSupport.scala
+++ modules/core/src/test/scala/com/leighperry/log4zio/testsupport/TestSupport.scala
@@ -2,37 +2,71 @@ package com.leighperry.log4zio.testsupport
 
 import org.scalacheck.Prop.forAll
 import org.scalacheck.util.Pretty
-import org.scalacheck.{Arbitrary, Gen, Prop, Shrink}
+import org.scalacheck.{ Arbitrary, Gen, Prop, Shrink }
 import zio.console.Console
-import zio.{DefaultRuntime, ZIO}
+import zio.{ DefaultRuntime, ZIO }
+
+final class TestSupportOps[A](val actual: A) {
+  def shouldBe(expected: A): Boolean = {
+    val result = expected == actual
+    if (!result) {
+      println(s"       => FAIL: expected[$expected]")
+      println(s"                  actual[$actual]")
+    }
+    result
+  }
+
+  def shouldSatisfy(f: A => Boolean): Boolean = {
+    val result = f(actual)
+    if (!result) {
+      println(s"       => FAIL:   doesn't satisfy, actual: [$actual]")
+    }
+    result
+  }
+
+}
+
+trait ToTestSupportOps {
+  implicit def `Ops for TestSupport`[A](actual: A): TestSupportOps[A] =
+    new TestSupportOps[A](actual)
+}
+
+////
 
 final class TestSupportZIOOps[E, A](val io: ZIO[Any, E, A]) {
-  def shouldBe(expected: A): ZIO[Console, E, Boolean] =
-    ZIO.accessM { env =>
-      io.flatMap { actual =>
-        val result = expected == actual
-        if (!result) {
-          env.console
-            .putStrLn(
-              s"""       => FAIL: expected[$expected]
-                 |                  actual[$actual]""".stripMargin
-            )
-            .map(_ => result)
-        } else {
-          ZIO.succeed(result)
+  def shouldBeIO(expected: A): ZIO[Console, E, Boolean] =
+    ZIO.accessM {
+      env =>
+        io.flatMap {
+          actual =>
+            val result = expected == actual
+            if (!result) {
+              env
+                .console
+                .putStrLn(
+                  s"""       => FAIL: expected[$expected]
+                     |                  actual[$actual]""".stripMargin
+                )
+                .map(_ => result)
+            } else {
+              ZIO.succeed(result)
+            }
         }
-      }
     }
-  def shouldSatisfy(f: A => Boolean): ZIO[Console, E, Boolean] =
-    ZIO.accessM { env =>
-      io.flatMap { actual =>
-        val result = f(actual)
-        if (!result) {
-          env.console
-            .putStrLn(s"       => FAIL:   doesn't satisfy, actual: [$actual]")
-            .map(_ => result)
-        } else ZIO.succeed(result)
-      }
+
+  def shouldSatisfyIO(f: A => Boolean): ZIO[Console, E, Boolean] =
+    ZIO.accessM {
+      env =>
+        io.flatMap {
+          actual =>
+            val result = f(actual)
+            if (!result) {
+              env
+                .console
+                .putStrLn(s"       => FAIL:   doesn't satisfy, actual: [$actual]")
+                .map(_ => result)
+            } else ZIO.succeed(result)
+        }
     }
 }
 
@@ -50,19 +84,19 @@ trait TestSupportGens {
   def genMultiline(genTestString: Gen[String]): Gen[(List[String], String)] =
     for {
       scount <- Gen.chooseNum[Int](0, 20)
-        strings <- Gen.listOfN(scount, genTestString)
+      strings <- Gen.listOfN(scount, genTestString)
     } yield strings -> strings.flatMap(s => List("\n", s, "\n")).mkString("\n")
 
   def genSymbol(minLength: Int, maxLength: Int): Gen[String] =
     for {
       n <- Gen.chooseNum(minLength, maxLength)
-        s <- Gen.listOfN(n, Gen.alphaChar).map(_.mkString)
+      s <- Gen.listOfN(n, Gen.alphaChar).map(_.mkString)
     } yield s
 
   def genNonEmptyString(maxLength: Int): Gen[String] =
     for {
       n <- Gen.chooseNum(1, maxLength)
-        s <- Gen.listOfN(n, Gen.asciiChar).map(_.mkString)
+      s <- Gen.listOfN(n, Gen.asciiChar).map(_.mkString)
     } yield s
 
   def genFor[A: Arbitrary]: Gen[A] =
@@ -72,8 +106,7 @@ trait TestSupportGens {
 
 ////
 
-trait TestSupportZIO
-  extends DefaultRuntime {
+trait TestSupportScalacheck extends DefaultRuntime {
 
   // ScalaCheck forAll except for ZIO
   def forAllZIO[T1, P, E](g1: Gen[T1])(f: T1 => ZIO[zio.ZEnv, E, P])(
@@ -81,8 +114,15 @@ trait TestSupportZIO
     s1: Shrink[T1],
     pp1: T1 => Pretty
   ): Prop =
-    forAll(g1) { t1 =>
-      unsafeRun(f(t1))
+    forAll(g1) {
+      t1 =>
+        unsafeRun(f(t1))
+    }
+
+  def simpleTest(condition: => Boolean): Prop =
+    forAll(implicitly[Arbitrary[Int]].arbitrary) {
+      _ =>
+        condition
     }
 
 }
@@ -90,9 +130,9 @@ trait TestSupportZIO
 ////
 
 trait TestSupport
-  extends ToTestSupportZIOOps
-    with TestSupportGens
-    with TestSupportZIO
+  extends ToTestSupportOps
+  with ToTestSupportZIOOps
+  with TestSupportGens
+  with TestSupportScalacheck
 
-object testsupportinstances
-  extends TestSupport
+object testsupportinstances extends TestSupport
