3933cebe6eb7a7db199d3b7f12f2f682753422b0
leigh-perry <lperry.breakpoint@gmail.com>
2019-10-19 22:46:40 +0000

    Sketch new API

--- modules/core/src/main/scala/com/leighperry/log4zio/Sketch.scala
+++ modules/core/src/main/scala/com/leighperry/log4zio/Sketch.scala
@@ -15,22 +15,9 @@ final case class LogWriter[A](log: A => UIO[Unit]) {
 
 }
 
-object RawLogger {
-  def console[A]: LogWriter[A] =
-    LogWriter[A](a => zio.console.Console.Live.console.putStrLn(a.toString)) // TODO toString?
-}
+////
 
-object TaggedLogger {
-
-  def console[A](prefix: Option[String]): LogWriter[(Level, String)] =
-    taggedMessageWriter(prefix).contramapM {
-      case (level, message) =>
-        ZIO
-          .effect(LocalDateTime.now)
-          .map(timestampFormat.format)
-          .catchAll(_ => UIO("(timestamp error)"))
-          .map(TaggedMessage(message, level, _))
-    }
+object Tagged {
 
   trait Level { val name: String }
   final object Error extends Level { override val name = "ERROR" }
@@ -38,48 +25,68 @@ object TaggedLogger {
   final object Info extends Level { override val name = "INFO" }
   final object Debug extends Level { override val name = "DEBUG" }
 
-  // TODO message: () => String
-  private final case class TaggedMessage[A](message: A, level: Level, timestamp: String)
+  final case class TaggedMessage[A](message: A, level: Level, timestamp: String)
+}
 
-  private def taggedMessageWriter[A](prefix: Option[String]): LogWriter[TaggedMessage[A]] =
-    RawLogger.console.contramap {
-      m =>
-        "%s %-5s - %s%s".format(
-          m.timestamp,
-          m.level.name,
-          prefix.fold("")(s => s"$s: "),
-          m.message
-        )
-    }
+//// output implementations
+
+object RawLogger {
+  def console[A]: LogWriter[A] =
+    LogWriter[A](a => zio.console.Console.Live.console.putStrLn(a.toString)) // TODO toString?
+}
+
+object TaggedStringLogWriter {
+  import Tagged._
+
+  def console(prefix: Option[String]): LogWriter[(Level, String)] =
+    RawLogger
+      .console
+      .contramap {
+        m: TaggedMessage[String] =>
+          "%s %-5s - %s%s".format(
+            m.timestamp,
+            m.level.name,
+            prefix.fold("")(s => s"$s: "),
+            m.message
+          )
+      }
+      .contramapM {
+        case (level, message) =>
+          ZIO
+            .effect(LocalDateTime.now)
+            .map(timestampFormat.format)
+            .catchAll(_ => UIO("(timestamp error)"))
+            .map(TaggedMessage(message, level, _))
+      }
 
   private val timestampFormat: DateTimeFormatter =
     DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
 }
 
-////
-
-final case class TaggedLogger(prefix: Option[String]) {
-
-  // TODO inject
-  private val logWriter = TaggedLogger.console[String](prefix)
+final class Logger private (logWriter: LogWriter[(Tagged.Level, String)]) {
 
   def error(prefix: Option[String], s: String): UIO[Unit] =
-    logWriter.log(TaggedLogger.Error -> s)
+    logWriter.log(Tagged.Error -> s)
 
   def warn(prefix: Option[String], s: String): UIO[Unit] =
-    logWriter.log(TaggedLogger.Warn -> s)
+    logWriter.log(Tagged.Warn -> s)
 
   def info(prefix: Option[String], s: String): UIO[Unit] =
-    logWriter.log(TaggedLogger.Info -> s)
+    logWriter.log(Tagged.Info -> s)
 
   def debug(prefix: Option[String], s: String): UIO[Unit] =
-    logWriter.log(TaggedLogger.Debug -> s)
+    logWriter.log(Tagged.Debug -> s)
 
+}
+
+object Logger {
+  def apply(logWriter: LogWriter[(Tagged.Level, String)]): Logger =
+    new Logger(logWriter)
 }
 
 object XXX extends zio.App {
 
-  val logger = TaggedLogger(Some("HEY"))
+  val logger = Logger(TaggedStringLogWriter.console(Some("an-app")))
 
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] =
     (logger.info(None, "someinfo") *> logger.error(None, "someerror"))
