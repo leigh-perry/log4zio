ee11a5d777a4dd161ec845f0c38d38f8ca8fab24
leigh-perry <lperry.breakpoint@gmail.com>
2019-10-20 04:11:53 +0000

    Finalised api and added examples

--- modules/slf4j/src/main/scala/com/leighperry/log4zio/slf4j/Slf4j.scala
+++ modules/slf4j/src/main/scala/com/leighperry/log4zio/slf4j/Slf4j.scala
@@ -1,50 +1,23 @@
 package com.leighperry.log4zio.slf4j
 
-import com.leighperry.log4zio.Log
-import zio.{ Task, UIO, ZIO }
+import com.leighperry.log4zio.{ Level, LogMedium, TaggedStringLogMedium }
+import zio.ZIO
 
-object Slf4j {
+object Slf4jLogMedium {
 
-//  /**
-//   * This implementation meets the Log.Service requirement logging does not emit failures, so it
-//   * implements fallback to console.
-//   */
-//  def slf4j[A](prefix: Option[A], fRenderA: A => String): ZIO[Any, Nothing, Log[A, String]] =
-//    ZIO.effect {
-//      org.slf4j.LoggerFactory.getLogger(getClass)
-//    }.map {
-//      slfLogger =>
-//        new Log[String, String] {
-//          override def log: Log.Service[String, String] =
-//            new Log.Service[A, String] {
-//              override def log(ls: Log.LogStep[A]): ZIO[Any, Nothing, Unit] = {
-//                def write(message: () => A, fOutput: String => Unit): Task[Unit] =
-//                  ZIO.effect(fOutput(fRenderA(message())))
-//
-//                val result: Task[Unit] =
-//                  ls match {
-//                    case Log.Error(message) =>
-//                      write(message, slfLogger.error)
-//                    case Log.Warn(message) =>
-//                      write(message, slfLogger.warn)
-//                    case Log.Info(message) =>
-//                      write(message, slfLogger.info)
-//                    case Log.Debug(message) =>
-//                      write(message, slfLogger.debug)
-//                  }
-//
-//                result.catchAll(_ => Log.console(prefix).flatMap(_.log.log(ls))) // fallback on write failure
-//              }
-//
-//              // TODO separate concerns log vs render/output
-//              override def render(ls: Log.LogStep[A]): String =
-//                ???
-//
-//              override def output(b: String): UIO[Unit] =
-//                ???
-//
-//            }
-//        }
-//    }.catchAll(_ => Log.console(prefix)) // fallback on creation failure
+//  val slf: org.slf4j.Logger = org.slf4j.LoggerFactory.getLogger(getClass)
 
+  def logger(prefix: Option[String], slf: org.slf4j.Logger): LogMedium[(Level, String)] =
+    LogMedium {
+      a =>
+        val (level, s) = a
+        val result =
+          level match {
+            case Level.Debug => ZIO.effect(slf.debug(s))
+            case Level.Error => ZIO.effect(slf.error(s))
+            case Level.Info => ZIO.effect(slf.info(s))
+            case Level.Warn => ZIO.effect(slf.warn(s))
+          }
+        result.catchAll(_ => TaggedStringLogMedium.console(prefix).log(a)) // fallback on write failure
+    }
 }
